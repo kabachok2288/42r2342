e36c434505869a60d2a65db310862dc156dbf600
kabachok2288 <72178231+kabachok2288@users.noreply.github.com>
2020-10-13 14:07:37 +0700

    13

---
+++ app.rb
@@ -0,0 +1,37 @@
+require 'sinatra'
+require 'pony'
+
+configure do
+  Pony.options = {
+    :headers => { 'Content-Type' => 'text/html' },
+    :via => :smtp,
+    :via_options => {
+      :address => 'smtp.sendgrid.net',
+      :port => '587',
+      :domain => 'heroku.com',
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :authentication => :plain,
+      :enable_starttls_auto => true
+    }
+  }
+end
+
+get '/' do
+  erb :index
+end
+
+post '/' do
+  Pony.mail(
+    :to => params[:to],
+    :from => params[:from],
+    :subject => params[:subject],
+    :body => nl2br(params[:body])
+  )
+  @sent = true
+  erb :index
+end
+
+def nl2br(string)
+  string.gsub("\n\r","<br>").gsub("\r", "").gsub("\n", "<br />")
+end
